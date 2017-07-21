e3ce42e5bf5749bb5277c54beaaf9ec4e06328f8
Emiliano <e.tortorelli1991@gmail.com>
2017-07-20 21:39:25 -0300

    test

---
+++ Android/AutoStalker/app/src/main/java/autostalker/bananaforscale/com/autostalker/Activities/SettingsActivity.java
@@ -0,0 +1,12 @@
+package autostalker.bananaforscale.com.autostalker.Activities;
+
+import android.app.Activity;
+
+/**
+ * Created by Emiliano on 20/07/2017.
+ */
+
+public class SettingsActivity extends Activity {
+
+
+}
