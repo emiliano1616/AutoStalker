a196bb0b593fed3d7fd1f82ed695e1a6af5c424a
rahnzo <alvarez.carlosalb@gmail.com>
2017-07-26 18:24:43 -0300

    Soporte para Gamepad en pantalla de conduccion

--- Android/AutoStalker/app/src/main/java/autostalker/bananaforscale/com/autostalker/Activities/DriveModeActivity.java
+++ Android/AutoStalker/app/src/main/java/autostalker/bananaforscale/com/autostalker/Activities/DriveModeActivity.java
@@ -2,33 +2,59 @@ package autostalker.bananaforscale.com.autostalker.Activities;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Intent;
+import android.hardware.input.InputManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.InputEvent;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.view.View.OnTouchListener;
 
 import com.google.vr.sdk.widgets.video.VrVideoEventListener;
 import com.google.vr.sdk.widgets.video.VrVideoView;
 
-import android.net.Uri;
-import android.widget.MediaController;
-
-import java.io.IOException;
+//import com.example.android.apis.R;
 
 import autostalker.bananaforscale.com.autostalker.R;
+import autostalker.bananaforscale.com.autostalker.Utils.CommonUtils;
 
-public class DriveModeActivity extends Activity {
+
+public class DriveModeActivity extends Activity implements InputManager.InputDeviceListener  {
 
     private VrVideoView mVrVideoView;
+    private static final String TAG = "GameControllerInput";
+    private InputManager mInputManager;
+
+    @Override
+    public boolean dispatchGenericMotionEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            getDirectionPressed(event);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Implementation of InputManager.InputDeviceListener.onInputDeviceRemoved()
+    @Override
+    public void onInputDeviceRemoved(int deviceId) {
+    }
+    // Implementation of InputManager.InputDeviceListener.onInputDeviceAdded()
+    @Override
+    public void onInputDeviceAdded(int deviceId) {
+
+    }
+    // Implementation of InputManager.InputDeviceListener.onInputDeviceChanged()
+    @Override
+    public void onInputDeviceChanged(int deviceId) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,33 +63,53 @@ public class DriveModeActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.drive_mode);
 
+        //initJoystick();
         initViews();
+    }
+
+    public int getDirectionPressed(InputEvent event) {
+
+        // If the input event is a MotionEvent, check its hat axis values.
+        if (event instanceof MotionEvent) {
+
+            // Use the hat axis value to find the D-pad direction
+            MotionEvent motionEvent = (MotionEvent) event;
+
+            /*if(motionEvent.getY() > 0) {
+                CommonUtils.showMessage("Accion:", "Freno", this);
+                return 0;
+            }*/
+
+//            CommonUtils.showMessage("Axis: Report", motionEvent.getX() + "|" + motionEvent.getY(), this.getContext());
+            Double angle = CommonUtils.getAngleByXYAxis(motionEvent.getX(), motionEvent.getY() * -1);
+            Double power = CommonUtils.getPowerPressByXYAxis(motionEvent.getX(), motionEvent.getY() * -1);
+
+            CommonUtils.showMessage("Angle and power ", angle.toString() + "|" + power.toString(), this);
+        }
+
+        return 0; //directionPressed;
     }
 
     private void initViews() {
         mVrVideoView = (VrVideoView) findViewById(R.id.video_view);
 
-        //String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
-        String vidAddress = "http://192.168.0.114:8200/MediaItems/21.mp4";
+        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        //String vidAddress = "http://192.168.0.114:8200/MediaItems/21.mp4";
         //String vidAddress = "http://192.168.0.114:8090?action=stream";
         //String vidAddress = "http://192.168.0.114:8081";
-        Uri vidUri = Uri.parse(vidAddress);
+        //Uri vidUri = Uri.parse(vidAddress);
 
         try {
             VrVideoView.Options options = new VrVideoView.Options();
             options.inputType = VrVideoView.Options.TYPE_MONO;
-            mVrVideoView.loadVideo(vidUri, options);
+            //mVrVideoView.loadVideo(vidUri, options);
             //mVrVideoView.loadVideoFromAsset("seaturtle.mp4", options);
-        } catch( IOException e ) {
+        } catch( Exception /*IOException */ e ) {
             //Handle exception
         }
 
         // Mensaje
-        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-        dlgAlert.setMessage("This is an alert with no consequence");
-        dlgAlert.setTitle("App Title");
-        dlgAlert.setPositiveButton("OK", null);
-        dlgAlert.setCancelable(true);
-        dlgAlert.create().show();
+
     }
+
 }
---
+++ Android/AutoStalker/app/src/main/java/autostalker/bananaforscale/com/autostalker/Utils/CommonUtils.java
@@ -0,0 +1,49 @@
+package autostalker.bananaforscale.com.autostalker.Utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import java.lang.Math;
+import java.lang.ref.Reference;
+
+import static android.R.attr.angle;
+import static android.os.Build.VERSION_CODES.M;
+
+public class CommonUtils {
+
+    public static void showMessage(String title, String msg, Context context){
+
+        //AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this.getContext());
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(context);
+        dlgAlert.setMessage(msg);
+        dlgAlert.setTitle(title);
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
+
+    public static double getAngleByXYAxis(float x, float y) {
+        double angle = Math.atan(y/x);
+        angle = Math.toDegrees(angle);
+
+        if (x<0)
+             angle += 180;
+        if (y<0 && x>0)
+            angle += 360;
+
+        return angle;
+        //return Math.toDegrees(angle);
+    }
+
+    public static double getPowerPressByXYAxis(float x, float y) {
+
+        if(y < 0) {
+            y = y * -1;
+        }
+        if(x < 0) {
+            x = x * -1;
+        }
+        double hip = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+        return hip / 1.41; // 1.41 the max  hip for 1,1 offset
+    }
+}
